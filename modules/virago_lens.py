e6f69b93e5ed245407a48c1900d89144cea30282
ajdevaux <32491625+ajdevaux@users.noreply.github.com>
2018-04-27 14:32:35 -0400

    optimizations

--- modules/virago_lens.py
+++ modules/virago_lens.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python3
 from future.builtins import input
 import numpy as np
 import pandas as pd
@@ -11,7 +11,9 @@ from bokeh.plotting import figure, curdoc, ColumnDataSource
 # from bokeh.models.widgets import Button, RadioButtonGroup, Select, Slider
 from skimage import io as skio
 from skimage import measure, img_as_int
-import ebovchan as ebc
+import vpipes
+# import holoviews as hv
+# hv.extension('bokeh')
 '''
 Use the ``bokeh serve`` command to run the example by executing:
     bokeh serve --show virago_lens.py
@@ -20,11 +22,11 @@ It will then ask you to input a directory containing the raw experimental data (
 '''
 
 # expt_dir = input("\nPlease type in the path to the folder that contains the IRIS data:\n")
-expt_dir = '/Volumes/KatahdinHD/ResilioSync/NEIDL/DATA/IRIS/pCHIP_results/pCHIP001_VSV-MARV@1E6'
+expt_dir = '/Volumes/KatahdinHD/ResilioSync/NEIDL/DATA/IRIS/tCHIP_results/tCHIP008_VSV-EBOVmay@1E6'
+
 expt_dir = expt_dir.strip('"')
 os.chdir(expt_dir)
-sample_name = ebc.sample_namer(expt_dir)
-
+sample_name = vpipes.sample_namer(expt_dir)
 
 txt_list = sorted(glob.glob('*.txt'))
 pgm_list = sorted(glob.glob('*.pgm'))
@@ -38,12 +40,12 @@ pic_dir = '../virago_output/' + chip_name + '/processed_images'
 vcount_dir =  '../virago_output/'+ chip_name + '/vcounts'
 
 image_list = sorted(glob.glob('*.pgm'))
-image_list, mirror = ebc.mirror_finder(image_list)
-data_select = chip_name + '.005.010'
+image_list, mirror = vpipes.mirror_finder(image_list)
+data_select = chip_name + '.001.001'
 image_set = sorted(list(set([".".join(image.split(".")[:3]) for image in image_list])))
 
 
-def load_image2(pic_dir, data_select):
+def load_image(pic_dir, data_select):
     os.chdir(pic_dir)
     pic_list = sorted(glob.glob('*.png'))
     for pic_name in pic_list:
@@ -52,10 +54,10 @@ def load_image2(pic_dir, data_select):
             pic = skio.imread(pic_name, as_grey = True)
 
     data_name = ".".join(data_name.split(".")[:-1])
-    img_final = measure.block_reduce(pic,block_size = (2,2))
+    pic = measure.block_reduce(pic,block_size = (2,2))
+    pic = pic[::-1, :]
 
-    return img_final, data_name
-
+    return pic, data_name
 
 def load_data(vcount_dir, data_name):
     os.chdir(vcount_dir)
@@ -69,42 +71,40 @@ def load_data(vcount_dir, data_name):
     pc = data.pc
     cv_bg = data.cv_bg
 
-    rzeros = np.zeros(len(pc))
-    data_dict = dict(x = x, y = y, z = z, pc = pc, cv_bg = cv_bg, select = pc*4)
+    return dict(x = x, y = y, z = z, pc = pc*2, cv_bg = cv_bg, select = ['red']*len(pc))
 
-    return data_dict
-
-img_final, data_name = load_image2(pic_dir, data_select)
-nrows, ncols = img_final.shape
+pic, data_name = load_image(pic_dir, data_select)
+nrows, ncols = pic.shape
 os.chdir(expt_dir)
 data_dict = load_data(vcount_dir, data_name)
-print("Done!")
+# data_dict = dict(x = 0, y = 0, z = 0, pc = 0, cv_bg = 0, select = 0)
 
 desc = Div(text=open(join(dirname(__file__), "description.html")).read(), width=300)
 
-# rzeros = np.zeros(len(data_dict['pc']))
-# rones = np.ones(len(data_dict['pc']))
 source = ColumnDataSource(data=data_dict)
 
 particle_data = HoverTool(tooltips = [("particle ID", "$index"),
                                       ("(x, y, z)", "(@x, @y, @z)"),
                                       ("percent contrast", "@pc"),
                                       ("background CV", "@cv_bg")
-                                     ])
-p = figure(plot_width = ncols, plot_height = nrows, min_border=10, min_border_left=5,
-          x_range=(0,ncols), y_range=(0,nrows), x_axis_location=None, y_axis_location=None,
-          tools = ['box_zoom,box_select,lasso_select,tap', particle_data, "reset"],
-          title = data_name)
-p.image(image=[img_final], x=-0.5, y=0, dw=(ncols - 0.5), dh=nrows)
-p.select(BoxSelectTool).select_every_mousemove = False
-p.select(LassoSelectTool).select_every_mousemove = False
-p.xgrid.grid_line_color = None
-p.ygrid.grid_line_color = None
+                                     ],
+                          )
 
-# all_circles = p.circle(x = 'x', y = 'y', size = 15, source = source,
-#                        fill_color = None, line_color = 'blue')
-all_circles = p.circle(x = 'x', y = 'y', size = 'select', source = source,
-                             fill_color = 'cyan', fill_alpha = 0.25, line_color = 'blue')
+spot = figure(plot_width = ncols, plot_height = nrows, min_border=10, min_border_left=5,
+              x_range=(0,ncols), y_range=(nrows,0), x_axis_location=None, y_axis_location=None,
+              tools = ['box_zoom,box_select,lasso_select,tap,reset',particle_data],
+              )
+
+
+spot.select(BoxSelectTool).select_every_mousemove = False
+spot.select(LassoSelectTool).select_every_mousemove = False
+spot.xgrid.grid_line_color = None
+spot.ygrid.grid_line_color = None
+
+all_circles = spot.circle(x = 'x', y = 'y', size = 'pc', source = source,
+                             fill_color = 'select', fill_alpha = 0.25, line_color = 'blue')
+
+spot.image(image=[pic], x=0, y=nrows, dw=ncols, dh=nrows)
 #************************************************************************************************#
 def load_histo(data_dict):
     bin_no = 100
@@ -127,11 +127,11 @@ histo_data = HoverTool(tooltips =[('bin','$index'),
                                   ],
                         )
 
-ph = figure(plot_width=p.plot_width, plot_height=180,
+ph = figure(plot_width=spot.plot_width, plot_height=180,
             x_range = DataRange1d(start = 0, follow = 'end', range_padding = 0.1),
             y_range = DataRange1d(start = 0, follow = 'end', range_padding = 0.25),
             min_border=10, min_border_left=5, y_axis_location='left',
-            tools = ['box_select,xbox_zoom,reset',histo_data,'tap'], toolbar_location = 'right',
+            tools = ['xbox_select,tap,reset',histo_data], toolbar_location = 'right',
             title = "Particle Contrast Histogram")
 
 
@@ -162,18 +162,17 @@ def data_reloader(select_img_data, dir):
 def update_particles(attrname, old, new):
     data_dict = data_reloader(select_img_data, vcount_dir)
     source.data = data_dict
-    all_circles = p.circle(x = 'x', y = 'y', size = 'select', source = source,
-                           fill_color = 'cyan', fill_alpha = 0.25, line_color = 'blue')
+    all_circles = spot.circle(x = 'x', y = 'y', size = 'pc', source = source,
+                                 fill_color = 'select', fill_alpha = 0.25, line_color = 'blue')
 
 def img_change(attrname, old, new):
     new_select = select_img_data.value
-    print(new_select)
     os.chdir(expt_dir)
     pic_dir = '../virago_output/' + chip_name + '/processed_images'
-    new_pic, data_name = load_image2(pic_dir, new_select)
+    new_pic, data_name = load_image(pic_dir, new_select)
     nrows, ncols = new_pic.shape
-    p.image(image=[new_pic], x=-0.5, y=0, dw=(ncols - 0.5), dh=nrows)
-    p.title.text = new_select
+    spot.image(image=[new_pic], x=0, y=nrows, dw=ncols, dh=nrows)
+    spot.title.text = new_select
 
 def update_histo(attrname, old, new):
     data_dict = data_reloader(select_img_data, vcount_dir)
@@ -206,9 +205,8 @@ def particle_select(attr, old, new):
     inds = np.array(new['1d']['indices'])
     data_dict = data_reloader(select_img_data, vcount_dir)
     histo_dict, hedges = load_histo(data_dict)
-    print(len(inds))
     if (len(inds) == len(data_dict['pc'])) or len(inds) == 0:
-        data_dict['select'] = data_dict['pc']*4
+        data_dict['select'] = ['red']*len(data_dict['pc'])
     # elif len(inds) == 0:
     #     data_dict['select'] = np.zeros(len(data_dict['pc']))
     else:
@@ -217,14 +215,13 @@ def particle_select(attr, old, new):
             selected_bin = (histo_dict['l_edges'][val], histo_dict['r_edges'][val])
             selected_bins.append(selected_bin)
         for val in selected_bins:
-            data_dict['select'][(data_dict['pc'] > val[0]) & (data_dict['pc'] <= val[1])] *= 4
-    print(data_dict['select'])
+            data_dict['select'][(data_dict['pc'] > val[0]) & (data_dict['pc'] <= val[1])] = 'yellow'
 
     source.data = data_dict
 
 main_histo.data_source.on_change('selected',particle_select)
 #************************************************************************************************#
 
-layout = row(column(desc, select_img_data), column(p, ph))
+layout = row(column(desc, select_img_data), column(spot, ph))
 curdoc().add_root(layout)
 curdoc().title = "VIRAGO LENS Viewer"
